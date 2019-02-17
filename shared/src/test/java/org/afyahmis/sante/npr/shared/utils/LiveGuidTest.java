52cc5633f6bd8927d8244e786e8ccc9203fc8346
Koske Kimutai <koskedk@live.com>
2019-02-17 22:16:29 +0300

    log4j config

--- shared/src/test/java/org/afyahmis/sante/npr/shared/utils/LiveGuidTest.java
+++ shared/src/test/java/org/afyahmis/sante/npr/shared/utils/LiveGuidTest.java
@@ -1,23 +1,31 @@
 package org.afyahmis.sante.npr.shared.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.UUID;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+
 
 public class LiveGuidTest {
-    Logger logger = LogManager.getRootLogger();
+    private static Logger logger = LogManager.getLogger();
 
     @Test
     public void should_generate_newId() {
         UUID idA=LiveGuid.NewId();
         UUID idB=LiveGuid.NewId();
         Assert.assertNotEquals(idA,idB);
+        logger.debug(idA);
+        logger.debug(idB);
     }
 
     @Test
     public void should_generate_random() {
+        UUID idA=LiveGuid.Random();
+        UUID idB=LiveGuid.Random();
+        Assert.assertNotEquals(idA,idB);
+        logger.debug(idA);
+        logger.debug(idB);
     }
 }
